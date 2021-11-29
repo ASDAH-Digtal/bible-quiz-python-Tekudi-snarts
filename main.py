d0b495fd42e926503239a5303b6880550fd96066
bird-boi <nielson_man@student.asdah.school.nz>
2021-11-30 10:45:27 +1300

    finished the basic code

--- main.py
+++ main.py
@@ -5,29 +5,209 @@ print("Come forth females and temales to this bible quiz")
 print("___________________________________________________________")
 print("Ten questions, Ten points.")
 print("___________________________________________________________")
-print("What are you doing? if you even think of getting this score you need some jesus in your life")
+print("0 - 3, What are you doing? if you even think of getting this score you need some jesus in your life")
 print("___________________________________________________________")
 print("Pretty mid score if you got 4 - 7.")
 print("___________________________________________________________")
 print("You're a nerd if you got 8 - 10 points")
 print("___________________________________________________________")
+print("When answering a question try to use one concise word or the letter infront of your answer. \n")
  
 while True:
- question_one = input(". Who was the first king of Israel? \n (a) Colossians\n (b) Hebrews\n (c) John\n (d) Revelation  \n>>")
-  correct_q1 = ["D","d","(d)","Revelation", "revelation"]
- incorrect_q1 = ["A","a","(a)","Colossians","colossians","B","b","(b)","Hebrews","hebrews","C","c","(c)","John","john"]
- 
- if question_one.lower() in correct_q1:
-   print("===========================================================")
-   print("Great Job, you got the answer right.")
-   score += 1
-   print("You now have: {} point".format(score))
-   break
- elif question_one.lower() in incorrect_q1:
-   print("Not the answer i'm looking for. Nice try.")
-   score += 0
-   print("You now have: {} point".format(score))
-   break
- else:
-   print("You did not choose one of the options")
-print("===========================================================")
+
+  question_one = input(". Who was the first king of Israel? \n (a) Saul\n (b) God\n (c) Me\n (d) David  \n>>")
+  correct_q1 = ["a","saul"]
+  incorrect_q1 = ["b","god","me", "c", "d", "david"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Correeeeeeecccccccccct.")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("lol noob. Try getting the next one correct")
+    score += 0
+    print("You now have: {} point".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". What was Elijah? \n (a) A Leader\n (b) A king\n (c) A prophet\n (d) Your dad  \n>>")
+  correct_q1 = ["c","prophet", "a prophet"]
+  incorrect_q1 = ["a","leader", "a leader" "king", "a king" "b", "d", "dad", "your dad"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("some people would have actually gotten that wrong.")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("Can't say I blame you but, I blame you")
+    score += 0
+    print("You now have: {} point".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". How many people did Jesus feed in one go? \n (a) 21\n (b) 5,000\n (c) 420\n (d) 119  \n>>")
+  correct_q1 = ["b","5000", "5,000"]
+  incorrect_q1 = ["a","21","420", "c", "420", "d", "119"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Good work.")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("If you said 21, then you have some of the worst faith I have seen, if you said 420 the answer was wrong but you will always be right to me")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". In creation, what came first? Heaven or the animals? \n (a) Heaven\n (b) The animals\n (c) Queen Elizabeth II\n (d) deez.  \n>>")
+  correct_q1 = ["a","heaven"]
+  incorrect_q1 = ["b","the animals", "animals", "queen elizabeth", "queen", "elizabeth", "c", "d", "deez"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("If you got that wrong then we would have a problem. Good thing you didn't")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("Guys. Come on, this is probably the easiest question. HAVE YOU EVEN READ THE BIBLE.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". Who was the cousin of Jesus? \n (a) The father\n (b) The son\n (c) The holy spirit\n (d) James.  \n>>")
+  correct_q1 = ["d","james"]
+  incorrect_q1 = ["a","the father", "father" "the son", "son", "the holy spirit", "spirit", "holy", "c", "b"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Good work on avoiding the trick")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("LOL YOU FELL FOR THE TRICK.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". Who or what did Moses lead? \n (a) pack of wolves\n (b) Israelites\n (c) clouds\n (d) Sheep.  \n>>")
+  correct_q1 = ["Israelites","b"]
+  incorrect_q1 = ["a","pack", "wolves", "pack of wolves", "clouds" "sheep", "c", "b"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("That is correct, nice")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("WOW, you know I can't tell if I'm shocketh or disappointed.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+while True:
+
+  question_one = input(". Was peter a tax collector? \n (a) yes\n (b) no\n (c) maybe\n (d) no clue.  \n>>")
+  correct_q1 = ["yes","a", "true"]
+  incorrect_q1 = ["c","maybe", "no clue", "b", "d", "no"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Very nice, round of applause for you, ok no more clapping, hurry up and move on to the next question")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("He  is litterally the fisher of man, dude I tried to make these easy for you..")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+while True:
+
+  question_one = input(". Who was the waifu of Abraham/Abram? \n (a) Bathsheeba\n (b) Naomi\n (c) Sarah/Sarai\n (d) Nico Robin.  \n>>")
+  correct_q1 = ["c","sarah", "sarai", "sarah/sarai"]
+  incorrect_q1 = ["a","bathsheeba", "nico robin", "d", "b", "naomi"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Pretty easy question but a point is a point notheless.")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("ok no. But if you picked 'Nico Robin', then I must say that you are a man of culture, although I cannot award you a point know that you will always have my respect.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+while True:
+
+  question_one = input(". Ok this one is very easy. Was David a shepherd or a king? \n (a) no\n (b) Shepherd\n (c) king\n (d) both.  \n>>")
+  correct_q1 = ["d","both",]
+  incorrect_q1 = ["a","no", "shepherd", "king", "b", "c "]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Phew I would have gone brain dead if you got that wrong, I mean you have to be pretty brain dead to get that wrong.")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("no.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+while True:
+
+  question_one = input(". Alright, this is the final question. Who was the wisest man in the bible? \n (a) me\n (b) you\n (c) Solomon\n (d) Salmon.  \n>>")
+  correct_q1 = ["Solomon","c",]
+  incorrect_q1 = ["a","me", "you", "salmon", "b", "d"]
+  if question_one.lower() in correct_q1:
+    print("===========================================================")
+    print("Looks like you are not .")
+    score += 1
+    print("You now have: {} points".format(score))
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("no.")
+    score += 0
+    print("You now have: {} points".format(score)) 
+    break
+  else:
+    print("Please remember to use the letters or one word for your answer. (TLDR: you didn't give an eligble answer)")
+  print("===========================================================")
+
+  
